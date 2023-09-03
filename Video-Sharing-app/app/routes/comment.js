ef9740e31351b9967e25be160c9f49bce3ce2e3b
Muhammad Mutahir <121317971+MRMutahir@users.noreply.github.com>
2023-09-03 00:34:27 -0700

    start delete comment work

---
+++ Video-Sharing-app/app/routes/comment.js
@@ -0,0 +1,9 @@
+import express from "express";
+import {verifytoken} from "../verifytoken.js"
+import { addcomment, getcomment, deletecomment } from "../Controllers/comment.js";
+const commentrouter = express.Router();
+commentrouter.post("/addcomment",verifytoken, addcomment);
+commentrouter.delete("/deletecomment",verifytoken, deletecomment);
+commentrouter.get("/find/:id", getcomment);
+
+export { commentrouter };
