0a7292cc779eddaadacbf9e3e6101aeed5e0bb59
MRMutahir <mutahirkareem820@gmail.com>
2023-09-20 06:34:24 -0700

    Frontend Start

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import Menu from "../src/Components/Menu"
+import Navbar from "../src/Components/Navbar"
+import './App.css'
+
+
+const Container = styled.div``;
+const Main = styled.div``;
+function App() {
+
+
+  return (
+    <Container>
+      <Menu />
+      <Main>
+        <Navbar />
+      </Main>
+
+    </Container>
+  )
+}
+
+export default App
