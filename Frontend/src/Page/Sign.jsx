14442f37c07aba3ad9d0e908b6b275baf4f690bc
MRMutahir <mutahirkareem820@gmail.com>
2023-09-22 07:44:18 -0700

    React Video Sharing App UI Design

--- Frontend/src/Page/Sign.jsx
+++ Frontend/src/Page/Sign.jsx
@@ -8,16 +8,88 @@ const Container = styled.div`
   height: calc(100vh - 56px);
   color: ${({ theme }) => theme.text};
 `;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-// const Container =  styled.div`
+  background-color: ${({ theme }) => theme.bgLighter};
+  border: 1px solid ${({ theme }) => theme.soft};
+  padding: 20px 50px;
+  gap: 10px;
+  /* height: 30vh; */
+`;
 
-// `
+const Title = styled.h1`
+  font-size: 24px;
+`;
+const SubTitle = styled.h2`
+  font-size: 20px;
+  font-weight: 300;
+`;
+const Input = styled.input`
+  border: 1px solid ${({ theme }) => theme.soft};
+  border-radius: 3px;
+  padding: 10px;
+  width: 100%;
+  background-color: transparent;
+  outline: none;
+`;
+const Button = styled.button`
+  border: none;
+  padding: 10px 20px;
+  border-radius: 3px;
+  font-weight: 500;
+  cursor: pointer;
+  background-color: ${({ theme }) => theme.soft};
+  color: ${({ theme }) => theme.textSoft};
+`;
 
-// const Container =  styled.div`
+const More = styled.div`
+  display: flex;
+
+  font-size: 12px;
+  margin-top: 10px;
+  padding: 10px;
+`;
+
+const Links = styled.div`
+  display: flex;
+  margin-left: 30px;
+`;
+
+const Link = styled.div`
+  margin-left: 30px;
+`;
+
+// const Container =  styld.div`
 
 // `
 function Sign() {
-  return <Container>Sign in</Container>;
+  return (
+    <Container>
+      <Wrapper>
+        <Title>Sign in</Title>
+        <SubTitle>to continue to LamaTube</SubTitle>
+        <Input placeholder="username" />
+        <Input type="password" placeholder="password" />
+        <Button>Sign in</Button>
+        <Title>or</Title>
+        <Input placeholder="username" />
+        <Input placeholder="email" />
+        <Input type="password" placeholder="password" />
+        <Button>Sign up</Button>
+      </Wrapper>
+      <More>
+        English (USA)
+        <Links>
+          <Link>Help</Link>
+          <Link>Privacy</Link>
+          <Link>Terms</Link>
+        </Links>
+      </More>
+    </Container>
+  );
 }
 
 export default Sign;
